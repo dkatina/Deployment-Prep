0aae4f896c257a0347ddb9e11378b4bfd999cdc2
dkatina <108095842+dkatina@users.noreply.github.com>
2024-07-31 19:59:55 -0400

    conditional URI

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class DevelopmentConfig:
     DEBUG = True
 
 class ProductionConfig:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') if os.environ.get('SQLALCHEMY_DATABASE_URI') else 'mysql+mysqlconnector://root:BAC146@localhost/bes_ecom'
     CACHE_TYPE = 'SimpleCache'
     DEBUG = False
     
