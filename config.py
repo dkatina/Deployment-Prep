0e374d8a0dcf1ec4698f8caaf2737003586938ec
dkatina <108095842+dkatina@users.noreply.github.com>
2024-07-31 17:27:19 -0400

    added sql_uri to env

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+import os
 
 class DevelopmentConfig:
     SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:BAC146@localhost/bes_ecom'
@@ -5,7 +6,7 @@ class DevelopmentConfig:
     DEBUG = True
 
 class ProductionConfig:
-    SQLALCHEMY_DATABASE_URI = 'postgresql://dylank:Be4wCWPRvleRBxyQzjux5HIs8NLRXiP9@dpg-cql7sa3qf0us73frf1n0-a.ohio-postgres.render.com/dbecomprep'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     CACHE_TYPE = 'SimpleCache'
     DEBUG = False
     
