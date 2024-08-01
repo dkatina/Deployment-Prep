77969c3322d7db9aea970bea969c3fe21dc48003
dkatina <108095842+dkatina@users.noreply.github.com>
2024-08-01 09:17:37 -0400

    conditional URI 1

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_cors import CORS
 from database import db
@@ -52,7 +53,7 @@ def rate_limit_config():
 
 
 
-app = create_app('ProductionConfig')
+app = create_app('ProductionConfig') if os.environ.get('SQLALCHEMY_DATABASE_URI') else create_app('DevelopmentConfig')
 
 with app.app_context():
     db.drop_all()
